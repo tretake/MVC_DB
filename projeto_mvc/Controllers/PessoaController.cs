f0e9ce90e7a05e26934f119e39095859a62d54d3
ruan <ruanlourencosilva.rj@gmail.com>
2023-10-12 00:42:39 -0300

    prototipo

--- projeto_mvc/Controllers/PessoaController.cs
+++ projeto_mvc/Controllers/PessoaController.cs
@@ -6,18 +6,15 @@ namespace projeto_mvc.Controllers
     public class PessoaController : Controller
     {
         private readonly ProjetoContext _context;
-        PessoaController(ProjetoContext context) 
+        public PessoaController(ProjetoContext context) 
         {
             _context = context;
         }
 
-
-
-
         public IActionResult Index()
         {
             var dados = _context.Pessoas.ToList();
-            return View();
+            return View(dados);
         }
     }
 }

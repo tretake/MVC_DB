8c92c62e8fa1aab6155f862f511cdf10ec017223
ruan <ruanlourencosilva.rj@gmail.com>
2023-10-12 18:29:50 -0300

    inserindo dados iniciais

--- projeto_mvc/Data/ProjetoDbInicializador.cs
+++ projeto_mvc/Data/ProjetoDbInicializador.cs
@@ -14,8 +14,110 @@ namespace projeto_mvc.Data
 
                 context.Database.EnsureCreated();
 
-                //Pessoas
+                //Desenvolvedor
+                if (!context.Desenvolvedores.Any())
+                {
+                    context.Desenvolvedores.AddRange(new List<Desenvolvedor>()
+                    {
+                        new Desenvolvedor()
+                        {
+                            LogoImg = "https://cdn.mobygames.com/logos/4021-nintendo-co-ltd.png",
+                            Nome = "Nintendo"
+                        },
+                        new Desenvolvedor()
+                        {
+                            LogoImg = "https://cdn.mobygames.com/logos/15569170-sega-corporation.png",
+                            Nome = "Sega"
+                        }
 
+                    });
+                    context.SaveChanges();
+                }
+
+                //Jogo
+                if (!context.Jogos.Any())
+                {
+                    context.Jogos.AddRange(new List<Jogo>()
+                    {
+                        new Jogo()
+                        {
+                            CapaImg = "https://cdn.mobygames.com/covers/4046688-the-legend-of-zelda-majoras-mask-nintendo-64-front-cover.jpg",
+                            Nome = "The Legend of Zelda: Majora's Mask",
+                            DataLancamento = DateTime.Now,
+                            Id_Dev = 1
+                        },
+                        new Jogo()
+                        {
+                            CapaImg = "https://cdn.mobygames.com/covers/4045638-the-legend-of-zelda-nes-front-cover.jpg",
+                            Nome = "The Legend of Zelda",
+                            DataLancamento = DateTime.Now,
+                            Id_Dev = 1
+                        },
+                        new Jogo()
+                        {
+                            CapaImg = "https://cdn.mobygames.com/covers/4756691-the-legend-of-zelda-ocarina-of-time-nintendo-64-front-cover.jpg",
+                            Nome = "The Legend of Zelda: Ocarina of Time",
+                            DataLancamento = DateTime.Now,
+                            Id_Dev = 1
+                        },
+
+                    }) ;
+                    context.SaveChanges();
+                }
+
+                
+
+
+                //Plataforma
+                if (!context.Plataformas.Any())
+                {
+                    context.Plataformas.AddRange(new List<Plataforma>()
+                    {
+                        new Plataforma()
+                        {
+                            LogoImg = "https://www.liblogo.com/img-logo/max/ne7246n50d-nes-logo-nintendo-entertainment-system-zeldapedia-.png",
+                            Nome = "NES",
+                            DataLancamento = DateTime.Now,
+                            Desenvolvedor = "Nintendo"
+                        },
+                        new Plataforma()
+                        {
+                            LogoImg = "https://upload.wikimedia.org/wikipedia/en/0/04/Nintendo_64_Logo.svg",
+                            Nome = "Nintendo 64",
+                            DataLancamento = DateTime.Now,
+                            Desenvolvedor = "nintendo"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                //PlataformaJogos
+                if (!context.PlataformaJogos.Any())
+                {
+                    context.PlataformaJogos.AddRange(new List<Plataforma_Jogo>()
+                    {
+                        new Plataforma_Jogo()
+                        {
+                            IdPlataforma = 2,
+                            IdJogo = 1
+                        },
+                        new Plataforma_Jogo()
+                        {
+                            IdPlataforma = 1,
+                            IdJogo = 2
+
+                        },
+                        new Plataforma_Jogo()
+                        {
+                            
+                            IdPlataforma = 2,
+                            IdJogo = 3
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                //Pessoas
                 if (!context.Pessoas.Any())
                 {
                     context.Pessoas.AddRange(new List<Pessoa>()
